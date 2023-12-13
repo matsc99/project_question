01db555cf0e6498e4ca388a8a4f115619bb45788
matsc99 <mateus.sousa.cruz@hotmail.com>
2023-12-13 15:53:19 -0300

    config botao

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_question/questao.dart';
+import 'package:project_question/resposta.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -29,9 +30,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntaSelecionada]),
-            ElevatedButton(child: Text('Resposta 1'), onPressed: _responder),
-            ElevatedButton(child: Text('Resposta 2'), onPressed: _responder),
-            ElevatedButton(child: Text('Resposta 3'), onPressed: _responder),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         ),
       ),
---
+++ lib/resposta.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  final String texto;
+  final void Function() onSelection;
+
+  Resposta(this.texto, this.onSelection);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: MaterialButton(
+        textColor: Colors.white,
+        color: Colors.blue,
+        child: Text(texto),
+        onPressed: onSelection,
+      ),
+    );
+  }
+}
